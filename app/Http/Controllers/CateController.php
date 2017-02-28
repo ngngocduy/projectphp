011e69f19ae82c1bffe534702ebad3a0217e7250
ngngocduy <duynn@bsdvn.com>
2017-02-28 20:23:32 +0700

    Update Category
    Update Category

--- app/Http/Controllers/CateController.php
+++ app/Http/Controllers/CateController.php
@@ -6,33 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\CateRequest;
 use App\Cate;
-
 class CateController extends Controller {
 
-	public function getList(){
+	public function getList () {
 		$data = Cate::select('id','name','parent_id')->orderBy('id','DESC')->get()->toArray();
 		return view('admin.cate.list',compact('data'));
 	}
-
 	public function getAdd () {
 		$parent = Cate::select('id','name','parent_id')->get()->toArray();
 		return view('admin.cate.add',compact('parent'));
 	}
-
-	public function postAdd(CateRequest $request){
+	public function postAdd (CateRequest $request) {
 		$cate = new Cate;
-		$cate->name = $request->txtCateName;
-		$cate->alias =changeTitle($request ->txtCateName);
-		$cate->order = $request->txtOrder;
-		$cate->parent_id = $request->sltParent;
-		$cate->keywords = $request->txtKeywords;
+		$cate->name        = $request->txtCateName;
+		$cate->alias       = changeTitle($request->txtCateName);
+		$cate->order       = $request->txtOrder;
+		$cate->parent_id   = $request->sltParent;
+		$cate->keywords    = $request->txtKeywords;
 		$cate->description = $request->txtDescription;
 		$cate->save();
-
 		return redirect()->route('admin.cate.list')->with(['flash_level'=>'success','flash_message'=>'Success !! Complete Add Category']);
 	}
-
-	public function getDelete($id){
+	public function getDelete ($id) {
 		$parent = Cate::where('parent_id',$id)->count();
 		if ($parent == 0) {
 			$cate = Cate::find($id);
@@ -46,18 +41,26 @@ class CateController extends Controller {
 			echo"'
 			</script>";
 		}
-		
 	}
-
-	public function getEdit($id){
+	public function getEdit ($id) {
 		$data = Cate::findOrFail($id)->toArray();
 		$parent = Cate::select('id','name','parent_id')->get()->toArray();
 		return view('admin.cate.edit',compact('parent','data'));
 	}
-
-	public function postEdit(){
-		
+	public function postEdit (Request $request,$id) {
+		$this->validate($request,
+			["txtCateName" => "required"],
+			["txtCateName.required"=> "Please Enter Name Category"]
+		);
+		$cate = Cate::find($id);
+		$cate->name        = $request->txtCateName;
+		$cate->alias       = changeTitle($request->txtCateName);
+		$cate->order       = $request->txtOrder;
+		$cate->parent_id   = $request->sltParent;
+		$cate->keywords    = $request->txtKeywords;
+		$cate->description = $request->txtDescription;
+		$cate->save();
+		return redirect()->route('admin.cate.list')->with(['flash_level'=>'success','flash_message'=>'Success !! Complete Edit Category']);
 	}
-
 
 }
